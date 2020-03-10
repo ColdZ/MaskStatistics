04602529df6d51d278c2f72e51d5a2b70848dc48
Sherlock ColdZ <sherlock.leng@laixion.com>
2020-03-11 07:42:21 +0800

    ADD 建立專案

---
+++ MaskStatistics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MaskStatistics
+//
+//  Created by ColdZ on 2020/3/11.
+//  Copyright © 2020 ColdZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
